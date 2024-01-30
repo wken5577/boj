668d57c1e4635084364c8b3e01140947e82616b9
Hyunkyu Lee <88573971+wken5577@users.noreply.github.com>
2024-01-30 20:07:47 +0900

    [Gold II] Title: 트리의 지름, Time: 964 ms, Memory: 95064 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -3,59 +3,60 @@ import java.util.*;
 
 public class Main {
     static int v;
-    static int max, node;
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {1, -1, 0, 0};
 
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
              BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))
         ) {
             StringTokenizer st;
-            Scanner sc = new Scanner(System.in);
-            v = sc.nextInt();
+            v = Integer.parseInt(br.readLine());
             List<List<int[]>> list = new ArrayList<>();
             for (int i = 0; i <= v; i++) {
                 list.add(new ArrayList<>());
             }
-            
+
             for (int i = 0; i < v; i++) {
-                int s = sc.nextInt();
+                st = new StringTokenizer(br.readLine());
+                int s = Integer.parseInt(st.nextToken());
                 List<int[]> integerList = list.get(s);
                 while (true){
-                    int e = sc.nextInt();
+                    int e = Integer.parseInt(st.nextToken());
                     if (e == -1)
                         break;
-                    int d = sc.nextInt();
+                    int d = Integer.parseInt(st.nextToken());
                     integerList.add(new int[] {e, d});
                 }
             }
-
-            boolean[] visit = new boolean[v + 1];
-            // visit[1] = true;
-            dfs(list, 1, 0, visit);
-
-            visit = new boolean[v + 1];
-            // visit[node] = true;
-            dfs(list, node, 0, visit);
-
-            System.out.println(max);
+            int[] max = searchMax(list, 1);
+            int[] difMax = searchMax(list, max[0]);
+            System.out.println(difMax[1]);
         }
     }
 
-    private static void dfs(List<List<int[]>> list, int s, int len, boolean[] visit) {
-        if (len > max){
-            max = len;
-            node = s;
-        }
+    private static int[] searchMax(List<List<int[]>> list, int s) {
+        int max = Integer.MIN_VALUE;
+        int node = -1;
+        PriorityQueue<int[]> queue = new PriorityQueue<>((o1, o2) -> o2[1] - o1[1]);
+        queue.add(new int[] {s, 0});
+        boolean[] visit = new boolean[v + 1];
         visit[s] = true;
-        List<int[]> nexts = list.get(s);
-        for (int[] next : nexts) {
-            if (!visit[next[0]]){
-                visit[next[0]] = true;
-                dfs(list, next[0], next[1] + len, visit);
+
+        while(!queue.isEmpty()){
+            int[] now = queue.poll();
+            if (max < now[1]){
+                node = now[0];
+                max = now[1];
+            }
+            List<int[]> nexts = list.get(now[0]);
+            for (int[] next : nexts) {
+                if (!visit[next[0]]){
+                    visit[next[0]] = true;
+                    queue.add(new int[] {next[0], now[1] + next[1]});
+                }
             }
         }
+
+        return new int[]{node, max};
     }
 
 }
