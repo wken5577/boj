9d7a8a00fc958d208538a7c350aac64a3396dbcb
Hyunkyu Lee <88573971+wken5577@users.noreply.github.com>
2024-01-31 21:48:11 +0900

    [Gold III] Title: 웜홀, Time: 852 ms, Memory: 26052 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.java
+++ 백준/Gold/1865. 웜홀/웜홀.java
@@ -1,120 +1,84 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
- 
-class Road {
-    int end;
-    int weight;
- 
-    Road(int end, int weight) {
-        this.end = end;
-        this.weight = weight;
+import java.io.*;
+import java.util.*;
+class Edge {
+    int from;    int to;    int time;
+    Edge(int from, int to, int time) {
+        this.from = from;
+        this.to = to;
+        this.time = time;
     }
 }
- 
+
+
 public class Main {
-    static int N, M, W;
-    static int[] dist;
-    static ArrayList<ArrayList<Road>> a;
+    static int n, m, w;
     static final int INF = 987654321;
- 
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
- 
-        int TC = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        while (TC-- > 0) {
-            st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
-            W = Integer.parseInt(st.nextToken());
- 
-            dist = new int[N + 1];
-            a = new ArrayList<>();
-            for (int i = 0; i <= N; i++) {
-                a.add(new ArrayList<>());
-            }
- 
-            for (int i = 0; i < M + W; i++) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))
+        ) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int tc = Integer.parseInt(st.nextToken());
+
+            for (int i = 0; i < tc; i++) {
                 st = new StringTokenizer(br.readLine());
-                int start = Integer.parseInt(st.nextToken());
-                int end = Integer.parseInt(st.nextToken());
-                int weight = Integer.parseInt(st.nextToken());
- 
-                if (i < M) { // 도로는 양방향 그래프
-                    a.get(start).add(new Road(end, weight));
-                    a.get(end).add(new Road(start, weight));
-                } else { // 웜홀은 단방향 그래프
-                    a.get(start).add(new Road(end, -weight));
-                }
-            }
- 
-            boolean isMinusCycle = false;
-            for (int i = 1; i <= N; i++) {
-                if (bellmanFord(i)) {
-                    isMinusCycle = true;
-                    sb.append("YES\n");
-                    break;
-                }
-            }
- 
-            if (!isMinusCycle) {
-                sb.append("NO\n");
-            }
-        }
- 
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-    
-    // 벨만포드 알고리즘
-    public static boolean bellmanFord(int start) {
-        Arrays.fill(dist, INF);
-        dist[start] = 0; // 시작점은 0으로 초기화.
-        boolean update = false;
-        
-        // (정점의 개수 - 1)번 동안 최단거리 초기화 작업을 반복함.
-        for (int i = 1; i < N; i++) {
-            update = false;
-            
-            // 최단거리 초기화.
-            for (int j = 1; j <= N; j++) {
-                for (Road road : a.get(j)) {
-                    if (dist[j] != INF && dist[road.end] > dist[j] + road.weight) {
-                        dist[road.end] = dist[j] + road.weight;
-                        update = true;
+                n = Integer.parseInt(st.nextToken());
+                m = Integer.parseInt(st.nextToken());
+                w = Integer.parseInt(st.nextToken());
+
+                ArrayList<Edge> edges = new ArrayList<>();
+
+
+                for (int j = 0; j < m + w; j++) {
+                    st = new StringTokenizer(br.readLine());
+                    int s = Integer.parseInt(st.nextToken());
+                    int e = Integer.parseInt(st.nextToken());
+                    int v = Integer.parseInt(st.nextToken());
+                    if (j >= m){
+                        edges.add(new Edge(s, e, -v));
+                    }else{
+                        edges.add(new Edge(s, e, v));
+                        edges.add(new Edge(e, s, v));
                     }
                 }
+
+                boolean cycle = false;
+                for (int j = 1; j <= n; j++) {
+                    cycle = bellman_ford( j, edges);
+                    if (cycle){
+                        break;
+                    }
+                }
+                System.out.println(cycle ? "YES" : "NO");
             }
-            
-            // 더 이상 최단거리 초기화가 일어나지 않았을 경우 반복문을 종료.
-            if (!update) {
-                break;
-            }
+
         }
-        
-        // (정점의 개수 - 1)번까지 계속 업데이트가 발생했을 경우
-        // (정점의 개수)번도 업데이트 발생하면 음수 사이클이 일어난 것을 의미함.
-        if (update) {
-            for (int i = 1; i <= N; i++) {
-                for (Road road : a.get(i)) {
-                    if (dist[i] != INF && dist[road.end] > dist[i] + road.weight) {
+    }
+
+    private static boolean bellman_ford(int start, ArrayList<Edge> edges) {
+        int[] dis = new int[n + 1];
+        Arrays.fill(dis, INF);
+        dis[start] = 0;
+        boolean update;
+
+        for (int i = 1; i <= n; i++) {
+            update = false;
+            for (int j = 0; j < edges.size(); j++) {
+                Edge cur = edges.get(j);
+                if (dis[cur.from] != INF && dis[cur.to] > dis[cur.from] + cur.time) {
+                    dis[cur.to] = dis[cur.from] + cur.time;
+                    update = true;
+                    if (i == n) {
                         return true;
                     }
                 }
+
             }
+            if (!update)
+                break;
         }
- 
+
         return false;
     }
- 
 }
