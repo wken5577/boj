fd9f5268fe349db5a7b83581150c1ab40f8761bb
Hyunkyu Lee <88573971+wken5577@users.noreply.github.com>
2024-04-11 13:54:36 +0900

    [Gold IV] Title: 팰린드롬?, Time: 788 ms, Memory: 211644 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -9,6 +9,7 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static int n, m;
+	static boolean[][] dy;
 
 	public static void main(String[] args) throws IOException {
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,30 +17,41 @@ public class Main {
 		) {
 			n = Integer.parseInt(br.readLine());
 			int[] arr = new int[n + 1];
+			dy = new boolean[n + 1][n + 1];
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int i = 1; i <= n; i++) {
 				arr[i] = Integer.parseInt(st.nextToken());
 			}
 			m = Integer.parseInt(br.readLine());
+			solution(arr);
 			while (m-- > 0) {
 				st = new StringTokenizer(br.readLine());
-				int lt = Integer.parseInt(st.nextToken());
-				int rt = Integer.parseInt(st.nextToken());
-				bw.write(solution(lt, rt, arr));
+				int s = Integer.parseInt(st.nextToken());
+				int e = Integer.parseInt(st.nextToken());
+				bw.write(dy[s][e] ? "1\n" : "0\n");
 			}
 		}
 
 	}
 
-	private static String solution(int lt, int rt, int[] arr) {
-		while (lt <= rt){
-			if (arr[lt] == arr[rt]){
-				lt++; rt--;
-			}else
-				return "0\n";
+	private static void solution(int[] arr) {
+		for (int i = 1; i <= n; i++) {
+			dy[i][i] = true;
+		}
+		for (int i = 1; i <= n - 1; i++) {
+			if (arr[i] == arr[i + 1])
+				dy[i][i + 1] = true;
 		}
 
-		return "1\n";
+		for(int i = 2; i <= n; i++){
+			int s = 1;
+			int e = s + i;
+			while (e <= n){
+				if (arr[s] == arr[e] && dy[s + 1][e - 1])
+					dy[s][e] = true;
+				s++; e++;
+			}
+		}
 	}
 
 }
