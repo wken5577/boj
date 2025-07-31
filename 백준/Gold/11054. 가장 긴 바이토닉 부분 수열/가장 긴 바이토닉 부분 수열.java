5578e10875be33c2974e05f66ab6123dfd88ea2e
Hyunkyu Lee <88573971+wken5577@users.noreply.github.com>
2025-07-31 21:46:42 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 132 ms, Memory: 14736 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -1,49 +1,54 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 public class Main {
-	static int n;
-
-	public static void main(String[] args) throws IOException {
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))
-		) {
-			n = Integer.parseInt(br.readLine());
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int[] arr = new int[n];
-			for (int i = 0; i < n; i++) {
-				arr[i] = Integer.parseInt(st.nextToken());
-			}
-
-			int[] LIS = new int[n];
-			int[] LDS = new int[n];
-
-			Arrays.fill(LIS, 1);
-			Arrays.fill(LDS, 1);
-
-			for (int i = 1; i < n; i++) {
-				for (int j = i - 1; j >= 0; j--) {
-					if (arr[i] > arr[j])
-						LIS[i] = Math.max(LIS[j] + 1, LIS[i]);
-				}
-			}
-
-			for (int i = n - 2; i >= 0; i--) {
-				for (int j = i + 1; j < n; j++) {
-					if (arr[i] > arr[j])
-						LDS[i] = Math.max(LDS[j] + 1, LDS[i]);
-				}
-			}
-
-			int answer = Integer.MIN_VALUE;
-			for (int i = 0; i < n; i++) {
-				answer = Math.max(answer, LIS[i] + LDS[i] - 1);
-			}
-			System.out.println(answer);
-		}
-	}
-
-
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = Integer.parseInt(br.readLine());
+            String[] input = br.readLine().split(" ");
+            int[] list = new int[n];
+            for (int i = 0; i < n; i++) {
+                list[i] = Integer.parseInt(input[i]);
+            }
+            Solution solution = new Solution();
+            System.out.println(solution.solution(n, list));
+        }
+    }
 }
 
+class Solution {
+    public int solution(int n, int[] list) {
+        int[] inc = new int[n];
+        int[] dec = new int[n];
+        inc[0] = 1;
+        dec[n - 1] = 1;
+
+        for (int i = 1; i < n; i++) {
+            int max = 0;
+            for (int j = i - 1; j >= 0; j--) {
+                if (list[i] > list[j]) {
+                    max = Math.max(max, inc[j]);
+                }
+            }
+            inc[i] = max + 1;
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            int max = 0;
+            for (int j = i + 1; j < n; j++) {
+                if (list[i] > list[j]) {
+                    max = Math.max(max, dec[j]);
+                }
+            }
+            dec[i] = max + 1;
+        }
+
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            answer = Math.max(answer, inc[i] + dec[i] - 1);
+        }
+        return answer;
+    }
+}
